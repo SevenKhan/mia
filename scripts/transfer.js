6cb33ad839cd69fa6605f8c0afaa0eaded6c5af4
SevenKhan <100589692+SevenKhan@users.noreply.github.com>
2024-07-30 22:23:38 +0000

    feat: initiated the Project

--- scripts/transfer.js
+++ scripts/transfer.js
@@ -16,11 +16,11 @@ const sendShieldedTransaction = async (signer, destination, data, value) => {
 async function main() {
   const contractAddress = fs.readFileSync("contract.txt", "utf8").trim();
   const [signer] = await hre.ethers.getSigners();
-  const contractFactory = await hre.ethers.getContractFactory("TestToken");
+  const contractFactory = await hre.ethers.getContractFactory("PERC20Sample");
   const contract = contractFactory.attach(contractAddress);
   const functionName = "transfer";
   const amount = 1 * 10 ** 18;
-  const functionArgs = ["0x3fcB34D7947d85BA1593D3e147b9bDfc10282e97", amount.toString()];
+  const functionArgs = ["0x16af037878a6cAce2Ea29d39A3757aC2F6F7aac1", amount.toString()];
   const transaction = await sendShieldedTransaction(
     signer,
     contractAddress,
